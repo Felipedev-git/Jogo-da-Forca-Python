5ed7ea6deb922d338c9aabc1c2177c5660fc3753
Felipedev-git <felipecampos932@gmail.com>
2025-11-27 12:43:50 -0300

    V2: Adicionado bloqueio de numeros, historico de repetidas e correcoes visuais

--- FORCA_v1.py
+++ FORCA_v1.py
@@ -1,5 +1,84 @@
 import random
 
+forca_desenhos = [
+    """
+       _______
+      |/      |
+      |     (x_x)
+      |      \|/
+      |       |
+      |      / \\
+      |
+    __|___
+    """,
+    # 1 vida
+    """
+       _______
+      |/      |
+      |      (_)
+      |      \|/
+      |       |
+      |      / 
+      |
+    __|___
+    """,
+    # 2 vidas
+    """
+       _______
+      |/      |
+      |      (_)
+      |      \|/
+      |       |
+      |
+      |
+    __|___
+    """,
+    # 3 vidas
+    """
+       _______
+      |/      |
+      |      (_)
+      |      \|/
+      |
+      |
+      |
+    __|___
+    """,
+    # 4 vidas
+    """
+       _______
+      |/      |
+      |      (_)
+      |      \|
+      |
+      |
+      |
+    __|___
+    """,
+    # 5 vidas
+    """
+       _______
+      |/      |
+      |      (_)
+      |
+      |
+      |
+      |
+    __|___
+    """,
+    # 6 vidas (Início)
+    """
+       _______
+      |/      |
+      |
+      |
+      |
+      |
+      |
+    __|___
+    """
+]
+
 palavras = [
     'ABACATE', 'ELEFANTE', 'COMPUTADOR', 'PROGRAMACAO', 'PYTHON',
     'GIRAFA', 'HIPOPOTAMO', 'CHOCOLATE', 'BRASIL', 'FUTEBOL',
@@ -11,9 +90,18 @@ palavras = [
 secreta = random.choice(palavras)
 lista = ["_"] * len(secreta)
 vidas = 6
+historico = []
 
 while True:
+    print(forca_desenhos[vidas])
     l1 = input('Chute uma letra: ').upper()
+    if not l1.isalpha():
+        print('Digite apenas letras! ')
+        continue
+    if l1 in historico:
+        print('Você já chutou essa, esqueceu?')
+        continue
+    historico.append(l1)
     for i in range(len(secreta)):
         if l1 == secreta[i]:
             lista[i] = l1            
@@ -25,6 +113,6 @@ while True:
         vidas = vidas - 1
         print(f'Restam {vidas} vidas!')
         if vidas == 0:
+            print(forca_desenhos[0])
             print(f'Game over! A palavra correta era {secreta}!')
-            break
-         
+            break    
